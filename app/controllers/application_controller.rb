825a15e2e072f226d36e1f5eb53e9b6a7046164b
vvdpzz <vvdpzz@gmail.com>
2011-10-27 21:41:06 +0800

    fix unlogin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!
+  layout :route_layout
   protect_from_forgery
+  
+  private
+    def route_layout
+      devise_controller? ? "welcome" : "application"
+    end
 end
