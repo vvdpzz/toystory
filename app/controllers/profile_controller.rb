152b7dd67a1d263e7e8c26fa1443f0f9b26c9550
feipinghuang <huang900107@163.con>
2011-10-27 14:47:08 +0800

    add profile methods

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,75 +1,85 @@
 class ProfileController < ApplicationController
+  before_filter :get_current_user_profile, :only => [:update_description, :update_location, :update_introduction, :update_website]
   def follow_user
-    if user = User.select("id,username,picture").find_by_id params[:uid]
-      $redis.sadd("users:#{current_user.id}.follow_users", params[:uid])
-      $redis.hset("users:#{current_user.id}.follow_users.info", params[:uid], MultiJson.encode(user.attributes.slice("id","username","picture")))
-      render :json => {:msg => "", :rc => 0}
+    if user = User.select("id,username,picture").find_by_id(params[:uid])
+      $redis.sadd("users:#{current_user.id}.following_users", params[:uid])
+      $redis.sadd("users:#{params[:uid]}.follower_users", current_user.id)
+      $redis.hset("users:#{current_user.id}.following_users.info", params[:uid], MultiJson.encode(user))
+      render json: {msg: "", rc: 0}
     else
-      render :json => {:msg => "关注失败", :rc => 1}
+      render json: {msg: "follow faild", rc: 1}
     end 
   end
   
   def unfollow_user
-    if User.select("id").find_by_id params[:uid] && $redis.sismember("users:#{user_id}.follow_users", params[:uid])
-      $redis.serm("users:#{current_user.id}.follow_users", params[:uid])
-      $redis.hdel("users:#{current_user.id}.follow_users.info", params[:udi])
-      render :json => {:result => [1, 1], :rc => 0}
+    if User.select("id").find_by_id(params[:uid]) && $redis.sismember("users:#{user_id}.following_users", params[:uid])
+      $redis.serm("users:#{current_user.id}.following_users", params[:uid])
+      $redis.serm("users:#{params[:uid]}.follower_users", current_user.id)
+      $redis.hdel("users:#{current_user.id}.following_users.info", params[:udi])
+      render json: {result: [1, 1], rc: 0}
     else
-      render :json => {:msg => "取消关注失败", :rc => 1}
+      render json: {msg: "unfollow faild", rc: 1}
     end
   end
   
   def follow_contest
-    if Contest.select("id").find_by_id params[:cid]
-      $redis.sadd("users:#{current_user.id}.follow_contests", params[:cid])
-      render :json => {:msg => "", :rc => 0}
+    if Contest.select("id").find_by_id(params[:cid])
+      $redis.sadd("users:#{current_user.id}.following_contests", params[:cid])
+      $redis.sadd("contests:#{params[:cid]}.follower_users", current_user.id)
+      render json: {msg: "", rc: 0}
     else
-      render :json => {:msg => "关注失败", :rc => 1}
+      render json: {msg: "follow faild", rc: 1}
     end
   end
   
   def unfollow_contest
-    if Contest.select("id").find_by_id params[:cid] && $redis.sismember("users:#{current_user.id}.follow_contests", params[:cid])
-      $redis.sadd("users:#{current_user.id}.follow_contests", params[:cid])
-      render :json => ""
+    if Contest.select("id").find_by_id(params[:cid]) && $redis.sismember("users:#{current_user.id}.following_contests", params[:cid])
+      $redis.serm("users:#{current_user.id}.following_contests", params[:cid])
+      $redis.serm("contests:#{params[:cid]}.follower_users", current_user.id)
+      render nothing: true
     else
-      render :json => {:msg => "关注失败", :rc => 1}
+      render json: {msg: "follow faild", rc: 1}
     end
   end
   
   def update_username
     if current_user.update_attribute(:username, params[:username])
-      render :json => {:result => current_user.username, rc => 0}
+      render json: {result: current_user.username, rc: 0}
     else
-      render :json => {:msg => "更新错误", rc => 1}
+      render json: {msg: "update error", rc: 1}
+    end
   end
   
   def update_description
-    if current_user.profile.update_attribute(:description, params[:description])
-      render :json => {:result => current_user.description, rc => 0}
+    if @profile.update_attribute(:description, params[:description])
+      render json: {result: @profile.description, rc: 0}
     else
-      render :json => {:msg => "更新错误", rc => 1}    
+      render json: {msg: "update error", rc: 1} 
+    end   
   end
   
   def update_location
-    if current_user.profile.update_attribute(:location, params[:location])
-      render :json => {:result => current_user.location, rc => 0}
+    if @profile.update_attribute(:location, params[:location])
+      render json: {result: @profile.location, rc: 0}
     else
-      render :json => {:msg => "更新错误", rc => 1}    
+      render json: {msg: "update error", rc: 1}   
+    end 
   end
    
   def update_introduction
-    if current_user.profile.update_attribute(:introduction, params[:introduction])
-      render :json => {:result => current_user.introduction, rc => 0}
+    if @profile.update_attribute(:introduction, params[:introduction])
+      render json: {result: @profile.introduction, rc: 0}
     else
-      render :json => {:msg => "更新错误", rc => 1}    
+      render json: {msg: "update error", rc: 1}   
+    end 
   end
   
   def update_website
-    if current_user.profile.update_attribute(:introduction, params[:introduction])
-      render :json => {:result => current_user.introduction, rc => 0}
+    if @profile.update_attribute(:introduction, params[:introduction])
+      render json: {result: @profile.introduction, rc: 0}
     else
-      render :json => {:msg => "更新错误", rc => 1}    
+      render json: {msg: "update error", rc: 1}
+    end    
   end
   
   def update_categories
@@ -107,4 +117,24 @@ class ProfileController < ApplicationController
   def verify_sms_code
     
   end
+  
+  def load_users
+    if params[:direction] == 1
+      uids = $redis.smembers("users:#{params[:uid]}.follower_users")
+    else
+      uids = $redis.smembers("users:#{params[:uid]}.following_users")
+    end
+    @users = User.find(uids, :include => :profile)
+    if @users.present?
+      render partial: "user", :collection => @users, layout: false
+    else
+      render nothing: true
+    end
+  end
+  
+  
+  protected
+    def get_current_user_profile
+      @profile = current_user.profile
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def is_followed?(uid)
+    $redis.sismember("users:#{current_user.id}.following_users", uid)
+  end
 end
