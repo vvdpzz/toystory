5ae2bd0687f5a50dae1817cd5855ebc1561ada6b
feipinghuang <huang900107@163.con>
2011-10-29 16:55:46 +0800

    add entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,12 @@
 class Entry < ActiveRecord::Base
+  include Extensions::UUID
+  
+  belongs_to :user, :counter_cache => true
+  belongs_to :contest, :counter_cache => true
+  
+  has_many :comments, :class_name => "Comment", :foreign_key => "pixar_id", :dependent => :destroy
+  
+  default_scope order("created_at DESC")
+  
+  acts_as_voteable
 end
